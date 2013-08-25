7929897a93fee45818ed39ec42976b6de0eb75da
Thomas Morris <thomas.imorris@gmail.com>
2013-08-26 08:50:36 +0900

    fixed a bug in install.sh

--- install.sh
+++ install.sh
@@ -9,10 +9,9 @@
 
  ########## Variables
 
- dir=~/dotfiles                    # dotfiles directory
- olddir=~/dotfiles_old             # old dotfiles backup directory
- files="bashrc vimrc vim zshrc oh-my-zsh"   # list of files/folders to
-                                            # symlink in homedir
+ dir=~/dotfiles             # dotfiles directory
+ olddir=~/dotfiles_old      # old dotfiles backup directory
+ files="bashrc vimrc"       # list of files/folders to symlink in homedir
 
  ##########
 
