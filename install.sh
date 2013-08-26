43c065d180d23f19e6c33ea8f104c8f7ead66bfd
Thomas Morris <thomas.imorris@gmail.com>
2013-08-26 09:00:18 +0900

    added autocrlf= input to gitconfig. added git config to repo.

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@
 
  dir=~/dotfiles             # dotfiles directory
  olddir=~/dotfiles_old      # old dotfiles backup directory
- files="bashrc vimrc"       # list of files/folders to symlink in homedir
+ files="bashrc vimrc gitconfig"       # list of files/folders to symlink in homedir
 
  ##########
 
