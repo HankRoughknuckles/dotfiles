74edefaaa61789f5394599e7ac9c0608304f72e4
Thomas Morris <thomas.imorris@gmail.com>
2013-12-13 09:36:40 +0200

    Fix directory bug in install-snippets

--- install-snippets.sh
+++ install-snippets.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MY_SNIPPETS=~/Dropbox/dotfiles/vim/core-snippets/*
+MY_SNIPPETS=~/Dropbox/dotfiles/vim/mySnippets/*
 OLD_FILES=~/.vim/bundle/snipmate-snippets/snippets/*
 DESTINATION_DIRECTORY=~/.vim/bundle/snipmate-snippets/snippets
 BACKUP_DIRECTORY=~/.vim/bundle/snipmate-snippets/snippets/backup
@@ -20,5 +20,6 @@ done
 echo "creating links from my snippets to $DESTINATION_DIRECTORY"
 for file in $MY_SNIPPETS
 do
+  echo "performing ln -s $file $DESTINATION_DIRECTORY"
   ln -s $file $DESTINATION_DIRECTORY
 done
