1ba6febc2ed2a1c07514c796d2559b6d2f4dad8f
Thomas Morris <thomas.imorris@gmail.com>
2015-01-13 07:38:55 +0200

    Also install .tmux.conf when ./install is run

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@
  olddir=~/dotfiles_old      # old dotfiles backup directory
 
  # list of files/folders to symlink in homedir
- files="bashrc bash_aliases gitconfig gitignore_global inputrc curlrc bash_profile rspec"       
+ files="bashrc bash_aliases gitconfig gitignore_global inputrc curlrc bash_profile rspec tmux.conf"       
  vim_dir="vim"
  vim_color_dir=$vim_dir/colors
  vimrc_name="vimrc"
